bfcd44d71fce90689ce1f6a8a99fd602b851d693
Leviatan1505 <ruben.terrazas1505@outlook.com>
2026-01-17 16:07:51 -0700

    BUG FIX

--- app/ayuda/page.tsx
+++ app/ayuda/page.tsx
@@ -1,8 +1,27 @@
-"use client";
+import { MessageCircle } from "lucide-react";
 
 export default function Page() {
   return (<div className="flex justify-center px-4 py-10">
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSefjWhBLqUYm2Qq0J1n2_Ve7_3HHaegikk8adQkxm2ya7-6sw/viewform?embedded=true" width="700" height="625" frameBorder="0" >Cargando…</iframe>
+  <div className="w-full max-w-3xl">
+    <div className="grid place-items-center bg-brand-emerald text-brand-khaki font-semibold">
+              Estamos trabajando en esta seccion para brindarte el mejor servicio
+            </div>
+    <div className="grid place-items-center bg-brand-emerald text-brand-khaki font-semibold">
+              Si buscas ayuda puedes comunicarte al consultorio: 656 827 2062
+            </div>
+
+<a
+  href="https://wa.me/5216568272062?text=Hola%20necesito%20ayuda%20psicológica"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="inline-flex items-center gap-2 rounded-2xl bg-[#25D366] px-5 py-3 text-white font-medium shadow-md hover:opacity-90 transition"
+>
+  <MessageCircle className="h-5 w-5" />
+  Contactar por WhatsApp
+</a>
+
+
+  </div>
 </div>
 )
 }
