9588e13ff94e2698eef7c76e5f38ab5fcf1cd47b
Leviatan1505 <ruben.terrazas1505@outlook.com>
2026-01-17 15:58:54 -0700

    fix bug

--- app/ayuda/page.tsx
+++ app/ayuda/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Page() {
   return (<div className="flex justify-center px-4 py-10">
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSefjWhBLqUYm2Qq0J1n2_Ve7_3HHaegikk8adQkxm2ya7-6sw/viewform?embedded=true" width="700" height="625" frameBorder="0" marginHeight="0" marginWidth="0">Cargando…</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSefjWhBLqUYm2Qq0J1n2_Ve7_3HHaegikk8adQkxm2ya7-6sw/viewform?embedded=true" width="700" height="625" frameBorder="0" >Cargando…</iframe>
 </div>
 )
 }
